7b6aed770d7354683d873f13e63834796c6651be
RitwikSingh28 <itsritwik26@gmail.com>
2022-12-23 19:53:32 +0530

    made the navbar responsive

--- components/header.js
+++ components/header.js
@@ -22,49 +22,23 @@ const Header = () => {
   }, [router]);
 
   return (
-    <div className="absolute flex justify-between w-[100%] px-10 py-5 items-center text-center text-xs text-white font-space-grotesk">
+    <div class="mx-auto flex p-5 md:flex-col text-[17px] flex-row items-center justify-between w-full font-space-grotesk">
       <img
-        className="min-w-[100px] w-auto"
+        className="min-w-[100px] w-[200px]"
         alt=""
         src="../hacknitp57.svg"
       />
-      <div className="flex flex-row justify-between items-center sm:text-xs">
-        <b className="cursor-pointer mx-4">About Us</b>
-        <b
-          className="cursor-pointer mx-4"
-          onClick={onTimelineTextClick}
-        >
-          Timeline
-        </b>
-        <b
-          className="cursor-pointer mx-4"
-          onClick={onRulesTextClick}
-        >
-          Rules
-        </b>
-        <b
-          className="cursor-pointer mx-4"
-          onClick={onSponsorsTextClick}
-        >
-          Sponsors
-        </b>
-        <b
-          className="cursor-pointer mx-4"
-          onClick={onContactUsTextClick}
-        >
-          Contact Us
-        </b>
-        <div className="cursor-pointer relative z-0 mx-4">
-          <img
-            className="rounded-[15px]"
-            alt=""
-            src="/rectangle-39715.svg"
-          />
-          <b className="absolute top-[20%] left-[0px] text-2xs inline-block mx-4 z-10 text-black">
-            Join Our Discord
-          </b>
-        </div>
-      </div>
+
+      <nav class="flex flex-wrap justify-center items-center md:mt-4">
+        <a class="mr-5 cursor-pointer md:mt-1 sm:mt-1">About Us</a>
+        <a class="mr-5 cursor-pointer md:mt-1 sm:mt-1">Timeline</a>
+        <a class="mr-5 cursor-pointer md:mt-1 sm:mt-1">Rules</a>
+        <a class="mr-5 cursor-pointer md:mt-1 sm:mt-1">Sponsors</a>
+        <a class="mr-5 cursor-pointer md:mt-1 sm:mt-1">Contact Us</a>
+        <button class="border-0 py-2 px-5 cursor-pointer focus:outline-none rounded-xl  md:mt-1 sm:mt-1 text-black bg-[#2BFF08]">
+          Join Discord
+        </button>
+      </nav>
     </div>
   );
 };
--- pages/homepage.js
+++ pages/homepage.js
@@ -44,33 +44,35 @@ const Homepage = () => {
         src="/anirudhxu4pz7gi9jyunsplash-16@2x.png"
         layout='fill'
       />
-      <Header />
-      <div className="absolute top-[25%] left-[100px] md:left-[5%] md:right-[5%] max-w-[800px] flex flex-col items-start md:items-center">
-        <img
-          className="h-[75%] w-[75%] md:h-[100%] md:w-[100%] my-2 max-w-full max-h-full [&.animate]:animate-[3s_ease_0s_1_normal_forwards_roll-in-left] opacity-[0]"
-          alt=""
-          src="../group-1303.svg"
-          data-animate-on-scroll
-        />
-        <h3
-          className="mb-8 md:text-[25px] text-lg font-bold font-noto-sans text-green-100 inline-block [&.animate]:animate-[3s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0]"
-          id="date"
-          data-animate-on-scroll
-        >
-          December 20,2022 - February 19,2023
-        </h3>
-        <h6
-          className="m-[0] md:text-[15px] bg-clip-text bg-black text-xs font-normal font-noto-sans text-left md:text-center inline-block w-[75%] md:w-[100%] [-webkit-text-stroke:3px_rgba(255,_255,_255,_0)] [&.animate]:animate-[2s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0]"
-          id="texr"
-          data-animate-on-scroll
-        >
-          The hack is only a small part of the competition. The real spirit lies
-          in building your network in the growing tech community and collaborating
-          with some of the finest minds of the nation. Just remember, taking the
-          Hackathons too seriously defeats the purpose. Save the seriousness for
-          pitching to investors. Keeping an open mind will open new doors, help
-          build new relationships and acquire new skills. This is HackNITP.
-        </h6>
+      <div className="absolute top-[0px] left-[0px] w-full">
+        <Header />
+        <div className="max-w-[800px] sm:w-[70%] flex flex-col items-start lg:items-center lg:ml-4 md:mx-10 sm:mx-10 mt-20">
+          <img
+            className="h-[75%] w-[75%] md:h-[100%] md:w-[100%] my-2 max-w-full max-h-full [&.animate]:animate-[3s_ease_0s_1_normal_forwards_roll-in-left] opacity-[0]"
+            alt=""
+            src="../group-1303.svg"
+            data-animate-on-scroll
+          />
+          <h3
+            className="mb-8 md:text-[25px] text-lg font-bold font-noto-sans text-green-100 inline-block [&.animate]:animate-[3s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0]"
+            id="date"
+            data-animate-on-scroll
+          >
+            December 20,2022 - February 19,2023
+          </h3>
+          <h6
+            className="m-[0] md:text-[15px] bg-clip-text bg-black text-xs font-normal font-noto-sans text-left md:text-center inline-block w-[75%] md:w-[100%] [-webkit-text-stroke:3px_rgba(255,_255,_255,_0)] [&.animate]:animate-[2s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0]"
+            id="texr"
+            data-animate-on-scroll
+          >
+            The hack is only a small part of the competition. The real spirit lies
+            in building your network in the growing tech community and collaborating
+            with some of the finest minds of the nation. Just remember, taking the
+            Hackathons too seriously defeats the purpose. Save the seriousness for
+            pitching to investors. Keeping an open mind will open new doors, help
+            build new relationships and acquire new skills. This is HackNITP.
+          </h6>
+        </div>
       </div>
     </div>
   );
