ff721eb5ff13a34ed5cea30b34872fd89c3ea112
RitwikSingh28 <itsritwik26@gmail.com>
2022-12-23 21:27:02 +0530

    fix themes section for desktop

--- pages/rules.js
+++ pages/rules.js
@@ -43,13 +43,13 @@ const Rules = () => {
       />
       <div className="absolute top-0 left flex flex-col w-full">
         <Header />
-        <main className="w-[100%] mt-[5%] flex flex-col justify-center items-center">
+        <main className="w-full mt-[5%] flex flex-col justify-between items-center">
           <section
-            className="relative w-[70%] text-left text-[inherit] text-white font-inherit md:w-[700px] md:[transform:rotate(-500deg)]"
+            className="relative w-[70%] text-left text-[inherit] text-white font-inherit md:w-[700px]"
             id="instructions"
           >
             <div
-              className="flex flex-col justify-center items-center rounded-xl bg-gray-1100 [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] lg:opacity-[0]"
+              className="flex flex-col justify-center items-center rounded-xl p-10 bg-gray-1100 [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] lg:opacity-[0]"
               id="instruction"
               data-animate-on-scroll
             >
@@ -94,92 +94,64 @@ const Rules = () => {
             </div>
           </section>
           <section
-            className="w-[1218px] h-[734px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0] text-center text-6xl text-black font-space-grotesk"
+            className="flex flex-col items-center justify-between w-full [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0] text-center text-6xl text-black font-space-grotesk"
             id="themes"
             data-animate-on-scroll
           >
-            <div className="top-[183px] left-[0px] w-[500px] h-[73px]">
-              <RectangleComponentofRule1 />
-              <h5
-                className="m-[0]  top-[0px] left-[0px] text-[inherit] font-bold font-inherit flex items-center justify-center w-[500px] h-[73px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] opacity-[0]"
-                data-animate-on-scroll
-              >
-                Theme name
-              </h5>
+            <h1
+              className="text-white font-bold font-noto-sans"
+              id="themes"
+            >
+              THEMES
+            </h1>
+            <div className="w-full flex my-5 flex-row justify-between items-center">
+              <div className="relative">
+                <RectangleComponentofRule1 isLeft={true}/>
+                <h5
+                  className="absolute top-[-20%] left-[30%] font-bold [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] opacity-[0]"
+                  data-animate-on-scroll
+                >
+                  Theme name
+                </h5>
+              </div>
+              <div className="relative">
+                <RectangleComponentofRule1 isLeft={false} />
+                <h5
+                  className="absolute top-[-20%] left-[30%] font-bold [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] opacity-[0]"
+                  data-animate-on-scroll
+                >
+                  Theme name
+                </h5>
+              </div>
             </div>
-            <div className=" top-[183px] left-[718px] w-[500px] h-[73px]">
-              <RectangleComponentOfrule2 />
-              <h5
-                className="m-[0]  top-[0px] left-[0px] text-[inherit] font-bold font-inherit flex items-center justify-center w-[500px] h-[73px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] opacity-[0] [&.animate]:lg:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] lg:opacity-[0]"
-                data-animate-on-scroll
-              >
-                Theme name
-              </h5>
+            <div className="w-full flex my-5 flex-row justify-between items-center">
+              <div className="relative">
+                <RectangleComponentofRule1 isLeft={true}/>
+                <h5
+                  className="absolute top-[-20%] left-[30%] font-bold [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] opacity-[0]"
+                  data-animate-on-scroll
+                >
+                  Theme name
+                </h5>
+              </div>
+              <div className="relative">
+                <RectangleComponentofRule1 isLeft={false} />
+                <h5
+                  className="absolute top-[-20%] left-[30%] font-bold [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] opacity-[0]"
+                  data-animate-on-scroll
+                >
+                  Theme name
+                </h5>
+              </div>
             </div>
-            <div className="top-[422px] left-[0px] w-[500px] h-[73px]">
-              <div
-                className=" top-[-29px] left-[-68px] rounded-[20px] bg-green-100 w-[600px] h-[130px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-left] opacity-[0]"
-                data-animate-on-scroll
-              />
-              <h5
-                className="m-[0]  top-[0px] left-[0px] text-[inherit] font-bold font-inherit flex items-center justify-center w-[500px] h-[73px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-left] opacity-[0]"
-                data-animate-on-scroll
-              >
-                Theme name
-              </h5>
-            </div>
-            <div className=" top-[422px] left-[718px] w-[500px] h-[73px]">
-              <div
-                className=" top-[-29px] left-[-35px] rounded-[20px] bg-green-100 w-[600px] h-[130px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] opacity-[0] [&.animate]:lg:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] lg:opacity-[0]"
-                data-animate-on-scroll
-              />
-              <h5
-                className="m-[0]  top-[0px] left-[0px] text-[inherit] font-bold font-inherit flex items-center justify-center w-[500px] h-[73px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] opacity-[0] [&.animate]:lg:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] lg:opacity-[0]"
-                data-animate-on-scroll
-              >
-                Theme name
-              </h5>
-            </div>
-            <div className=" top-[661px] left-[0px] w-[500px] h-[73px]">
-              <div
-                className=" top-[-29px] left-[-68px] rounded-[20px] bg-green-100 w-[600px] h-[130px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-left] opacity-[0]"
-                data-animate-on-scroll
-              />
-              <h5
-                className="m-[0]  top-[0px] left-[0px] text-[inherit] font-bold font-inherit flex items-center justify-center w-[500px] h-[73px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-left] opacity-[0]"
-                data-animate-on-scroll
-              >
-                Theme name
-              </h5>
-            </div>
-            <div className=" top-[661px] left-[718px] w-[500px] h-[73px]">
-              <div
-                className=" top-[-29px] left-[-35px] rounded-[20px] bg-green-100 w-[600px] h-[130px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] opacity-[0] [&.animate]:lg:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] lg:opacity-[0]"
-                id="theme6"
-                data-animate-on-scroll
-              />
-              <h5
-                className="m-[0]  top-[0px] left-[0px] text-[inherit] font-bold font-inherit flex items-center justify-center w-[500px] h-[73px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] opacity-[0] [&.animate]:lg:animate-[1s_ease_0s_1_normal_forwards_slide-in-right] lg:opacity-[0]"
-                data-animate-on-scroll
-              >
-                Theme name
-              </h5>
-            </div>
-            <div className=" top-[0px] left-[350px] w-[516px] h-[54px] text-white font-noto-sans">
-              <h1
-                className="m-[0]  top-[0px] left-[0px] text-[inherit] font-bold font-inherit inline-block w-[516px] h-[54px] mix-blend-normal"
-                id="themes"
-              >
-                THEMES
-              </h1>
-            </div>
+            
           </section>
           <section
-            className="relative w-[70%] h-[864px] text-left text-[inherit] text-white font-inherit md:w-[700px] md:[transform:rotate(-500deg)]"
+            className="mt-20 relative w-[70%] h-[864px] text-left text-[inherit] text-white font-inherit md:w-[700px]"
             id="instructions"
           >
             <div
-              className="flex flex-col justify-center items-center rounded-xl bg-gray-1100 [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] lg:opacity-[0]"
+              className="flex flex-col p-10 justify-center items-center rounded-xl bg-gray-1100 [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] lg:opacity-[0]"
               id="instruction"
               data-animate-on-scroll
             >
@@ -187,42 +159,49 @@ const Rules = () => {
                 className="m-[0] text-6xl font-bold font-noto-sans text-center [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_slide-in-top] lg:opacity-[0]"
                 data-animate-on-scroll
               >
-                PRIZES & PERKS 
+                PRIZES & PERKS
               </h1>
               <ul className="w-[85%]">
                 <li>
                   <h3 className="m-[0] text-[inherit] font-normal font-inherit">
-                    Cash prize worth 20k sponsored by Quidnunc Infotainment and River Rises
+                    Cash prize worth 20k sponsored by Quidnunc Infotainment and
+                    River Rises
                   </h3>
                 </li>
                 <li>
                   <h3 className="m-[0] text-[inherit] font-normal font-inherit">
-                    Cash prize worth 20k sponsored by Quidnunc Infotainment and River Rises
+                    Cash prize worth 20k sponsored by Quidnunc Infotainment and
+                    River Rises
                   </h3>
                 </li>
                 <li>
                   <h3 className="m-[0] text-[inherit] font-normal font-inherit">
-                    Cash prize worth 20k sponsored by Quidnunc Infotainment and River Rises
+                    Cash prize worth 20k sponsored by Quidnunc Infotainment and
+                    River Rises
                   </h3>
                 </li>
                 <li>
                   <h3 className="m-[0] text-[inherit] font-normal font-inherit">
-                    Cash prize worth 20k sponsored by Quidnunc Infotainment and River Rises
+                    Cash prize worth 20k sponsored by Quidnunc Infotainment and
+                    River Rises
                   </h3>
                 </li>
                 <li>
                   <h3 className="m-[0] text-[inherit] font-normal font-inherit">
-                    Cash prize worth 20k sponsored by Quidnunc Infotainment and River Rises
+                    Cash prize worth 20k sponsored by Quidnunc Infotainment and
+                    River Rises
                   </h3>
                 </li>
                 <li>
                   <h3 className="m-[0] text-[inherit] font-normal font-inherit">
-                    Cash prize worth 20k sponsored by Quidnunc Infotainment and River Rises
+                    Cash prize worth 20k sponsored by Quidnunc Infotainment and
+                    River Rises
                   </h3>
                 </li>
                 <li>
                   <h3 className="m-[0] text-[inherit] font-normal font-inherit">
-                    Cash prize worth 20k sponsored by Quidnunc Infotainment and River Rises
+                    Cash prize worth 20k sponsored by Quidnunc Infotainment and
+                    River Rises
                   </h3>
                 </li>
               </ul>
