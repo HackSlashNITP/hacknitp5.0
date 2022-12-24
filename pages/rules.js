f4b6ecc4d55e149002f19e6e247654be23b98079
RitwikSingh28 <itsritwik26@gmail.com>
2022-12-24 12:04:06 +0530

    minor-fix in rules page

--- pages/rules.js
+++ pages/rules.js
@@ -38,134 +38,135 @@ const Rules = () => {
   return (
     <div className="relative bg-black w-full h-screen overflow-y-auto text-center text-xs text-white font-space-grotesk">
       <Image
-        // className="absolute top-[0px] left-[0px]"
         alt=""
         src={backgroundImage}
       />
-      <div className="absolute top-0 left flex flex-col w-full">
-        <Header />
-        <main className="w-full my-[5%] flex flex-col justify-between items-center">
-          <section
-            className="w-[70%] text-left text-white font-inherit md:w-[100%] sm:w-[100%]"
-            id="instructions"
-          >
-            <div
-              className="flex flex-col justify-center items-center rounded-xl p-10 bg-gray-1100 [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] lg:opacity-[0]"
-              id="instruction"
-              data-animate-on-scroll
+      <div className="absolute top-0 left flex flex-col w-full h-">
+        <div className="wrapper min-h-full mb-[-0px]">
+          <Header />
+          <main className="w-full my-[5%] flex flex-col justify-between items-center">
+            <section
+              className="w-[70%] text-left text-white font-inherit md:w-[100%] sm:w-[100%]"
+              id="instructions"
             >
-              <h1
-                className="m-[0] text-6xl sm:text-xl md:text-xl font-bold font-noto-sans text-center [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_slide-in-top] lg:opacity-[0]"
+              <div
+                className="flex flex-col justify-center items-center rounded-xl p-10 bg-gray-1100 [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] lg:opacity-[0]"
+                id="instruction"
                 data-animate-on-scroll
               >
-                INSTRUCTIONS
-              </h1>
-              <ul className="w-[85%] md:w-[100%] sm:w-[100%] md:text-[15px] sm:text-[15px] flex-col justify-center">
-                <li>
-                  <h3 className="m-2 md:m-[5px] sm:m-[5px] font-normal font-inherit">
-                    Participants can take part only as a team. A team must
-                    comprise of at least 2 members and at most 6.
-                  </h3>
-                </li>
-                <li>
-                  <h3 className="m-2 font-normal font-inherit">
-                    Participants can take part only as a team. A team must
-                    comprise of at least 2 members and at most 6.
-                  </h3>
-                </li>
-                <li>
-                  <h3 className="m-2 font-normal font-inherit">
-                    Participants can take part only as a team. A team must
-                    comprise of at least 2 members and at most 6.
-                  </h3>
-                </li>
-              </ul>
-              <button
-                className="cursor-pointer text-[#59db8a] text-base sm:text-xs md:text-xs [border:2px_solid_#59db8a] p-3 bg-[transparent] rounded-xl box-border [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_slide-in-left] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_slide-in-left] lg:opacity-[0]"
-                data-animate-on-scroll
-              >
-                Download Rulebook
-              </button>
-            </div>
-          </section>
-          <section
-            className="flex flex-col items-center justify-between w-full md:mx-auto sm:mx-auto [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0] text-center text-6xl text-black font-space-grotesk"
-            id="themes"
-            data-animate-on-scroll
-          >
-            <h1
-              className="text-white font-bold font-noto-sans md:text-[40px] sm:text-[40px]"
+                <h1
+                  className="m-[0] text-6xl sm:text-xl md:text-xl font-bold font-noto-sans text-center [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_slide-in-top] lg:opacity-[0]"
+                  data-animate-on-scroll
+                >
+                  INSTRUCTIONS
+                </h1>
+                <ul className="w-[85%] md:w-[100%] sm:w-[100%] md:text-[15px] sm:text-[15px] flex-col justify-center">
+                  <li>
+                    <h3 className="m-2 md:m-[5px] sm:m-[5px] font-normal font-inherit">
+                      Participants can take part only as a team. A team must
+                      comprise of at least 2 members and at most 6.
+                    </h3>
+                  </li>
+                  <li>
+                    <h3 className="m-2 font-normal font-inherit">
+                      Participants can take part only as a team. A team must
+                      comprise of at least 2 members and at most 6.
+                    </h3>
+                  </li>
+                  <li>
+                    <h3 className="m-2 font-normal font-inherit">
+                      Participants can take part only as a team. A team must
+                      comprise of at least 2 members and at most 6.
+                    </h3>
+                  </li>
+                </ul>
+                <button
+                  className="cursor-pointer text-[#59db8a] text-base sm:text-xs md:text-xs [border:2px_solid_#59db8a] p-3 bg-[transparent] rounded-xl box-border [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_slide-in-left] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_slide-in-left] lg:opacity-[0]"
+                  data-animate-on-scroll
+                >
+                  Download Rulebook
+                </button>
+              </div>
+            </section>
+            <section
+              className="flex flex-col items-center justify-between w-full md:mx-auto sm:mx-auto [&.animate]:animate-[1s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0] text-center text-6xl text-black font-space-grotesk"
               id="themes"
-            >
-              THEMES
-            </h1>
-
-            <div className="w-full sm:text-[25px] md:text-[25px]">
-              <div className="flex w-full justify-between sm:flex-col md:flex-col">
-                <div className="bg-[#2BFF08] my-4 p-5 flex justify-center items-center w-[40%] sm:w-[100%] md:w-[100%]">
-                  <b className="text-black text-lg">Theme</b>
-                </div>
-                <div className="bg-[#2BFF08] my-4 p-5 flex justify-center items-center w-[40%] sm:w-[100%] md:w-[100%]">
-                  <b className="text-black text-lg">Theme</b>
-                </div>
-              </div>
-              <div className="flex w-full justify-between sm:flex-col md:flex-col">
-                <div className="bg-[#2BFF08] my-4 p-5 flex justify-center items-center w-[40%] sm:w-[100%] md:w-[100%]">
-                  <b className="text-black text-lg">Theme</b>
-                </div>
-                <div className="bg-[#2BFF08] my-4 p-5 flex justify-center items-center w-[40%] sm:w-[100%] md:w-[100%]">
-                  <b className="text-black text-lg">Theme</b>
-                </div>
-              </div>
-            </div>
-          </section>
-          <section
-            className="w-[70%] mt-10 text-left text-white font-inherit md:w-[100%] sm:w-[100%]"
-            id="prizes-section"
-          >
-            <div
-              className="flex flex-col justify-center items-center rounded-xl p-10 bg-gray-1100 [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] lg:opacity-[0]"
-              id="prizes"
               data-animate-on-scroll
             >
               <h1
-                className="m-[0] text-6xl sm:text-xl md:text-xl font-bold font-noto-sans text-center [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_slide-in-top] lg:opacity-[0]"
+                className="text-white font-bold font-noto-sans md:text-[40px] sm:text-[40px]"
+                id="themes"
+              >
+                THEMES
+              </h1>
+
+              <div className="w-full sm:text-[25px] md:text-[25px]">
+                <div className="flex w-full justify-between sm:flex-col md:flex-col">
+                  <div className="bg-[#2BFF08] my-4 p-5 flex justify-center items-center w-[40%] sm:w-[100%] md:w-[100%]">
+                    <b className="text-black text-lg">Theme</b>
+                  </div>
+                  <div className="bg-[#2BFF08] my-4 p-5 flex justify-center items-center w-[40%] sm:w-[100%] md:w-[100%]">
+                    <b className="text-black text-lg">Theme</b>
+                  </div>
+                </div>
+                <div className="flex w-full justify-between sm:flex-col md:flex-col">
+                  <div className="bg-[#2BFF08] my-4 p-5 flex justify-center items-center w-[40%] sm:w-[100%] md:w-[100%]">
+                    <b className="text-black text-lg">Theme</b>
+                  </div>
+                  <div className="bg-[#2BFF08] my-4 p-5 flex justify-center items-center w-[40%] sm:w-[100%] md:w-[100%]">
+                    <b className="text-black text-lg">Theme</b>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section
+              className="w-[70%] mt-10 text-left text-white font-inherit md:w-[100%] sm:w-[100%]"
+              id="prizes-section"
+            >
+              <div
+                className="flex flex-col justify-center items-center rounded-xl p-10 bg-gray-1100 [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_fade-in] lg:opacity-[0]"
+                id="prizes"
                 data-animate-on-scroll
               >
-                PRIZES & PERKS
-              </h1>
-              <ul className="w-[85%] md:w-[100%] sm:w-[100%] md:text-[15px] sm:text-[15px] flex-col justify-center">
-                <li>
-                  <h3 className="m-2 md:mx-[5px] sm:mx-[5px] font-normal font-inherit">
-                    Cash prize worth 10k for best hack built on top of Ethereum
-                    and for teams that integrate Polygon in their hacks as well
-                    the prize money will be 15k along with eligibility for
-                    internship/full-time role interviews and a chance to land
-                    seed funding of upto 5000 USD(or equivalent)!*.
-                  </h3>
-                </li>
-                <li>
-                  <h3 className="m-2 md:mx-[5px] sm:mx-[5px] font-normal font-inherit">
-                    Cash prize worth 10k for best hack built on top of Ethereum
-                    and for teams that integrate Polygon in their hacks as well
-                    the prize money will be 15k along with eligibility for
-                    internship/full-time role interviews and a chance to land
-                    seed funding of upto 5000 USD(or equivalent)!*.
-                  </h3>
-                </li>
-                <li>
-                  <h3 className="m-2 md:mx-[5px] sm:mx-[5px] font-normal font-inherit">
-                    Cash prize worth 10k for best hack built on top of Ethereum
-                    and for teams that integrate Polygon in their hacks as well
-                    the prize money will be 15k along with eligibility for
-                    internship/full-time role interviews and a chance to land
-                    seed funding of upto 5000 USD(or equivalent)!*.
-                  </h3>
-                </li>
-              </ul>
-            </div>
-          </section>
-        </main>
+                <h1
+                  className="m-[0] text-6xl sm:text-xl md:text-xl font-bold font-noto-sans text-center [&.animate]:animate-[0.75s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0] [&.animate]:lg:animate-[0.75s_ease_0s_1_normal_forwards_slide-in-top] lg:opacity-[0]"
+                  data-animate-on-scroll
+                >
+                  PRIZES & PERKS
+                </h1>
+                <ul className="w-[85%] md:w-[100%] sm:w-[100%] md:text-[15px] sm:text-[15px] flex-col justify-center">
+                  <li>
+                    <h3 className="m-2 md:mx-[5px] sm:mx-[5px] font-normal font-inherit">
+                      Cash prize worth 10k for best hack built on top of Ethereum
+                      and for teams that integrate Polygon in their hacks as well
+                      the prize money will be 15k along with eligibility for
+                      internship/full-time role interviews and a chance to land
+                      seed funding of upto 5000 USD(or equivalent)!*.
+                    </h3>
+                  </li>
+                  <li>
+                    <h3 className="m-2 md:mx-[5px] sm:mx-[5px] font-normal font-inherit">
+                      Cash prize worth 10k for best hack built on top of Ethereum
+                      and for teams that integrate Polygon in their hacks as well
+                      the prize money will be 15k along with eligibility for
+                      internship/full-time role interviews and a chance to land
+                      seed funding of upto 5000 USD(or equivalent)!*.
+                    </h3>
+                  </li>
+                  <li>
+                    <h3 className="m-2 md:mx-[5px] sm:mx-[5px] font-normal font-inherit">
+                      Cash prize worth 10k for best hack built on top of Ethereum
+                      and for teams that integrate Polygon in their hacks as well
+                      the prize money will be 15k along with eligibility for
+                      internship/full-time role interviews and a chance to land
+                      seed funding of upto 5000 USD(or equivalent)!*.
+                    </h3>
+                  </li>
+                </ul>
+              </div>
+            </section>
+          </main>
+        </div>
         <Footer />
       </div>
     </div>
