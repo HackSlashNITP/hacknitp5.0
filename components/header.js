91319fddac13a606af8618f1541b9d0b0d9573b6
RitwikSingh28 <itsritwik26@gmail.com>
2022-12-24 12:00:39 +0530

    minor fixes

--- components/header.js
+++ components/header.js
@@ -21,20 +21,20 @@ const Header = () => {
   }, [router]);
 
   return (
-    <div class="mx-auto flex flex-wrap p-5 md:flex-col text-[17px] flex-row items-center justify-between w-full font-space-grotesk">
+    <div className="mx-auto flex flex-wrap p-5 md:flex-col text-[17px] flex-row items-center justify-between w-full font-space-grotesk">
       <img
         className="min-w-[100px] w-[200px]"
         alt=""
         src="../hacknitp57.svg"
       />
 
-      <nav class="flex flex-wrap justify-center items-center md:mt-4">
-        <b class="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={() => {}}>About Us</b>
-        <b class="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={onTimelineTextClick}>Timeline</b>
-        <b class="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={onRulesTextClick}>Rules</b>
-        <b class="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={() => {}}>Sponsors</b>
-        <b class="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={onContactUsTextClick}>Contact Us</b>
-        <button class="border-0 py-2 px-5 cursor-pointer focus:outline-none rounded-xl  md:mt-1 sm:mt-1 text-black bg-[#2BFF08]">
+      <nav className="flex flex-wrap justify-center items-center md:mt-4">
+        <b className="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={() => {}}>About Us</b>
+        <b className="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={onTimelineTextClick}>Timeline</b>
+        <b className="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={onRulesTextClick}>Rules</b>
+        <b className="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={() => {}}>Sponsors</b>
+        <b className="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={onContactUsTextClick}>Contact Us</b>
+        <button className="border-0 py-2 px-5 cursor-pointer focus:outline-none rounded-xl  md:mt-1 sm:mt-1 text-black bg-[#2BFF08]">
           Join Discord
         </button>
       </nav>
--- pages/homepage.js
+++ pages/homepage.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { useEffect } from "react";
 import Header from "../components/header";
+import bgImage from "../public/anirudhxu4pz7gi9jyunsplash-16@2x.png";
+import Footer from "../components/footer";
 
 const Homepage = () => {
   useEffect(() => {
@@ -35,44 +37,46 @@ const Homepage = () => {
 
   return (
     <div
-      className="relative bg-gray-1000 w-full h-screen overflow-y-auto text-center text-5xl text-white font-space-grotesk"
+      className="relative bg-black w-full h-screen overflow-y-auto text-center text-5xl text-white font-space-grotesk"
       id="homepage"
     >
       <Image
-        className="absolute top-[0px] left-[0px] object-cover"
+        className="absolute top-[0px] left-[0px]"
         alt=""
-        src="/anirudhxu4pz7gi9jyunsplash-16@2x.png"
-        layout='fill'
+        src={bgImage}
       />
-      <div className="absolute top-[0px] left-[0px] w-full">
-        <Header />
-        <div className="max-w-[800px] sm:w-[70%] flex flex-col items-start lg:items-center md:mx-10 sm:mx-10 mx-[10%] mt-20">
-          <img
-            className="h-[75%] w-[75%] md:h-[100%] md:w-[100%] my-2 max-w-full max-h-full [&.animate]:animate-[3s_ease_0s_1_normal_forwards_roll-in-left] opacity-[0]"
-            alt=""
-            src="../group-1303.svg"
-            data-animate-on-scroll
-          />
-          <h3
-            className="mb-8 md:text-[25px] text-lg font-bold font-noto-sans text-green-100 inline-block [&.animate]:animate-[3s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0]"
-            id="date"
-            data-animate-on-scroll
-          >
-            December 20,2022 - February 19,2023
-          </h3>
-          <h6
-            className="m-[0] md:text-[15px] bg-clip-text bg-black text-xs font-normal font-noto-sans text-left md:text-center inline-block w-[75%] md:w-[100%] [-webkit-text-stroke:3px_rgba(255,_255,_255,_0)] [&.animate]:animate-[2s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0]"
-            id="texr"
-            data-animate-on-scroll
-          >
-            The hack is only a small part of the competition. The real spirit lies
-            in building your network in the growing tech community and collaborating
-            with some of the finest minds of the nation. Just remember, taking the
-            Hackathons too seriously defeats the purpose. Save the seriousness for
-            pitching to investors. Keeping an open mind will open new doors, help
-            build new relationships and acquire new skills. This is HackNITP.
-          </h6>
+      <div className="absolute top-[0px] left-[0px] w-full h-screen flex flex-col justify-between">
+        <div className="wrapper min-h-full mb-[-0px]">
+          <Header />
+          <div className="max-w-[800px] sm:w-[70%] flex flex-col items-start lg:items-center md:mx-10 sm:mx-10 mx-[10%] my-10">
+            <img
+              className="h-[75%] w-[75%] md:h-[100%] md:w-[100%] my-2 max-w-full max-h-full [&.animate]:animate-[3s_ease_0s_1_normal_forwards_roll-in-left] opacity-[0]"
+              alt=""
+              src="../group-1303.svg"
+              data-animate-on-scroll
+            />
+            <h3
+              className="mb-8 md:text-[25px] text-lg font-bold font-noto-sans text-green-100 inline-block [&.animate]:animate-[3s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0]"
+              id="date"
+              data-animate-on-scroll
+            >
+              December 20,2022 - February 19,2023
+            </h3>
+            <h6
+              className="m-[0] md:text-[15px] bg-clip-text bg-black text-xs font-normal font-noto-sans text-left md:text-center inline-block w-[75%] md:w-[100%] [-webkit-text-stroke:3px_rgba(255,_255,_255,_0)] [&.animate]:animate-[2s_ease_0s_1_normal_forwards_slide-in-top] opacity-[0]"
+              id="texr"
+              data-animate-on-scroll
+            >
+              The hack is only a small part of the competition. The real spirit lies
+              in building your network in the growing tech community and collaborating
+              with some of the finest minds of the nation. Just remember, taking the
+              Hackathons too seriously defeats the purpose. Save the seriousness for
+              pitching to investors. Keeping an open mind will open new doors, help
+              build new relationships and acquire new skills. This is HackNITP.
+            </h6>
+          </div>
         </div>
+        <Footer />
       </div>
     </div>
   );
