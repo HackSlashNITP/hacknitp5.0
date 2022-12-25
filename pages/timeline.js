1e9f36e6bd5ed295de80cb2caa05865b38f869cc
Shashank_yadav <yadavshashank0907@gmail.com>
2022-12-26 00:56:16 +0530

    handling timeline page and transition effect in rule page

--- pages/timeline.js
+++ pages/timeline.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import DigitalCountdown from '../components/digitalCountdown'
 import Scheduling from '../components/scheduling'
-
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 const timeline = () => {
   return (
     <div>
+      <Header/>
       <DigitalCountdown/>
       <Scheduling/>
+      <Footer/>
     </div>
   )
 }
