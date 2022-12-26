2c56a09b98223399c68654c108ed78a5e7d6ef9f
RitwikSingh28 <itsritwik26@gmail.com>
2022-12-26 09:47:11 +0530

    make navbar image responsive

--- components/header.js
+++ components/header.js
@@ -31,20 +31,20 @@ const Header = () => {
   return (
     <div className="flex flex-wap p-5 md:flex-col md:justify-center md:items-center sm:flex-col sm:justify-center sm:items-center text-[17px] text-white flex-row items-center justify-between  font-space-grotesk">
       <img
-        className="min-w-[100px] w-auto cursor-pointer"
+        className="min-w-[150px] max-w-[250px] w-[20%] cursor-pointer"
         alt=""
         src="../hacknitp57.svg"
         onClick={onLogoClick}
       />
 
-      <nav className="flex mt-1 flex-wrap justify-center items-center text-xs gap-x-5 gap-y-1 font-semibold md:text-[16px] sm:text-[16px]">
+      <nav className="flex mt-1 flex-wrap justify-center items-center text-xs gap-x-5 gap-y-1 font-semibold md:text-[14px] sm:text-[14px]">
         <b className="cursor-pointer" onClick={onAboutUsClick}>About Us</b>
         <b className="cursor-pointer" onClick={onTimelineTextClick}>Timeline</b>
         <b className="cursor-pointer" onClick={onRulesTextClick}>Rules</b>
         <b className="cursor-pointer" onClick={onSponsorsTextClick}>Sponsors</b>
         <b className="cursor-pointer" onClick={onContactUsTextClick}>Contact Us</b>
         <a href="https://bit.ly/hacknitp-4-discord" target={"_blank"} rel="noreferrer noopener">
-          <button className="border-0 font-semibold py-2 px-5 text-xs cursor-pointer md:text-[16px] hover:bg-[#28e706] sm:text-[16px] focus:outline-none rounded-xl  md:mt-1 sm:mt-1 text-black bg-[#2BFF08]">
+          <button className="border-0 font-semibold py-2 px-5 text-xs cursor-pointer md:text-[14px] hover:bg-[#28e706] sm:text-[14px] focus:outline-none rounded-xl  md:mt-1 sm:mt-1 text-black bg-[#2BFF08]">
             Join Discord
           </button>
         </a>
