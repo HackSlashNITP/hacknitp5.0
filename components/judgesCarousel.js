9007a949a1195207d31e494d19c224c98d7f664c
RitwikSingh28 <itsritwik26@gmail.com>
2022-12-26 11:31:44 +0530

    add prev year judges in carousel slids

--- components/judgesCarousel.js
+++ components/judgesCarousel.js
@@ -1,74 +1,73 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from "../styles/judgesCarousel.module.css"
-import photo from "../public/model.png"
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/judgesCarousel.module.css";
 
 const JudgesCarousel = () => {
+  const judges = [
+    {
+      id: 1,
+      url: "/judges/Abhinav.jpeg",
+    },
+    {
+      id: 2,
+      url: "/judges/Akash.jpg",
+    },
+    {
+      id: 3,
+      url: "/judges/Ameer Jhan.jpg",
+    },
+    {
+      id: 4,
+      url: "/judges/arko.jpeg",
+    },
+    {
+      id: 5,
+      url: "/judges/Dominic.png",
+    },
+    {
+      id: 6,
+      url: "/judges/Kaustubh.jpeg",
+    },
+    {
+      id: 7,
+      url: "/judges/kaustubh.jpg",
+    },
+    {
+      id: 8,
+      url: "/judges/MadhavBahl.jpeg",
+    },
+    {
+      id: 9,
+      url: "/judges/ManishKumar.png",
+    },
+    {
+      id: 10,
+      url: "/judges/Sameer.png",
+    },
+    {
+      id: 11,
+      url: "/judges/Shreya.jpeg",
+    },
+    {
+      id: 12,
+      url: "/judges/Vinit.png",
+    },
+  ];
   return (
     <section className={styles.container}>
-        <div className={styles.slider}>
-           <div className={styles.slide_track}>
-
-           <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
+      <div className={styles.slider}>
+        <div className={styles.slide_track}>
+          {judges.map((judge) => {
+            return (
               <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
+                <Image className={styles.images} src={judge.url} key={judge.id} layout="fill"/>
               </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              <div className={styles.slide}>
-                <Image  className={styles.images} src={photo}/>
-              </div>
-              
-
-           </div>
+            );
+          })}
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default JudgesCarousel
+export default JudgesCarousel;
--- pages/about-us.js
+++ pages/about-us.js
@@ -3,7 +3,6 @@ import Image from "next/image";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import bgImage from "../public/anirudhxu4pz7gi9jyunsplash-11@2x.png";
-import Carousel from "../components/carousel";
 import JudgesCarousel from "../components/judgesCarousel";
 
 const AboutUs = () => {
@@ -59,10 +58,10 @@ const AboutUs = () => {
       <div className="absolute w-full h-screen top-0 left-0">
         <div className="wrapper min-h-full mb-[-0px]">
           <Header />
-          <div className="content w-full h-full flex flex-col justify-start items-center ">
+          <div className="content w-full h-full flex flex-col justify-start items-center md:justify-center sm:justify-center">
             <div className="about-us-info my-10">
-              <h1 className="text-5xl md:text-xl sm:text-xl">ABOUT US</h1>
-              <div className="w-full flex justify-start my-4 md:w-[90%] sm:w-[90%]">
+              <h1 className="text-5xl md:text-[25px] sm:text-[25px]">ABOUT US</h1>
+              <div className="w-full flex justify-start my-4 md:w-full sm:w-full sm:text-[15px] md:text-[16px]">
                 <div className="bg-[#1C172F] p-5 w-[50%] rounded-4xl md:w-full sm:w-full">
                   <p>
                     National Institute of Technology Patna is the 18th National
@@ -72,8 +71,8 @@ const AboutUs = () => {
                   </p>
                 </div>
               </div>
-              <div className="w-full flex justify-end my-4 md:w-[90%] sm:w-[90%]">
-                <div className="bg-[#1C172F] p-5 w-[50%] rounded-4xl md:w-full sm:w-full">
+              <div className="w-full flex justify-end my-4 md:w-full sm:w-full">
+                <div className="bg-[#1C172F] p-5 w-[50%] rounded-4xl md:w-full sm:w-full sm:text-[15px] md:text-[16px]">
                   <p>
                     National Institute of Technology Patna is the 18th National
                     Institute of Technology created by the Ministry of H.R.D.
@@ -83,78 +82,10 @@ const AboutUs = () => {
                 </div>
               </div>
             </div>
-            <div className="about-us-judges w-full flex flex-col justify-center items-center">
-              <h1 className="text-5xl md:text-xl sm:text-xl my-10">JUDGES</h1>
-              {/* <Carousel images={images} /> */}
+            <div className="about-us-judges w-full flex flex-col justify-center items-center my-3">
+              <h1 className="text-5xl md:text-[25px] sm:text-[25px] my-10 md:my-0 sm:my-0">PAST-YEAR JUDGES</h1>
               <JudgesCarousel/>
             </div>
-            {/* <h3
-                className="m-[0]  top-[1959px] left-[472px] text-6xl font-bold font-noto-sans inline-block w-[336px] h-[54px] [&.animate]:animate-[4s_ease_0s_1_normal_forwards_fade-in] opacity-[0]"
-                id="judges"
-                data-animate-on-scroll
-              >
-                JUDGES
-              </h3>
-              <div
-                className=" top-[1225px] left-[99px] w-[1082px] h-[655px] [&.animate]:animate-[4s_ease_0s_1_normal_forwards_fade-in] opacity-[0]"
-                id="box2"
-                data-animate-on-scroll
-              >
-                <img
-                  className=" top-[0px] left-[0px] rounded-[25px] w-[1082px] h-[585.8px] object-cover"
-                  alt=""
-                  src="../1892-2@2x.png"
-                />
-                <img
-                  className=" top-[246.08px] left-[981.63px] w-[66px] h-[66px]"
-                  alt=""
-                  src="../iconlybulkarrow--right-circle.svg"
-                />
-                <img
-                  className=" top-[246.32px] left-[28.93px] w-[66px] h-[66px]"
-                  alt=""
-                  src="../iconlybulkarrow--left-circle.svg"
-                />
-                <img
-                  className=" top-[637px] left-[459px] w-[18px] h-[18px]"
-                  alt=""
-                  src="../ellipse-9.svg"
-                />
-                <img
-                  className=" top-[637px] left-[495.72px] w-[18px] h-[18px]"
-                  alt=""
-                  src="../ellipse-10.svg"
-                />
-                <img
-                  className=" top-[637px] left-[532.44px] w-[18px] h-[18px]"
-                  alt=""
-                  src="../ellipse-11.svg"
-                />
-                <img
-                  className=" top-[637px] left-[569.16px] w-[18px] h-[18px]"
-                  alt=""
-                  src="../ellipse-12.svg"
-                />
-                <img
-                  className=" top-[637px] left-[605.88px] w-[18px] h-[18px]"
-                  alt=""
-                  src="../ellipse-13.svg"
-                />
-              </div>
-              <p className="m-[0]  top-[2457px] left-[423px] text-4xl font-bold inline-block w-[437px]">
-                Judge Name
-              </p>
-              <span
-                className=" top-[2510px] left-[423px] text-base inline-block w-[437px]"
-                id="post"
-              >
-                CEO Bhagwa Gang NITP
-              </span>
-              <img
-                className=" top-[2541px] left-[0px] w-[1280px] h-[339px] overflow-hidden"
-                alt=""
-                src="../frame-26.svg"
-              /> */}
           </div>
         </div>
         <Footer />
