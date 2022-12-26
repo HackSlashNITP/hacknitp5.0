928935afe0fe520c63edaf029a6e5ec3efc513c0
RitwikSingh28 <itsritwik26@gmail.com>
2022-12-26 09:13:14 +0530

    made navbar better

--- components/header.js
+++ components/header.js
@@ -29,22 +29,22 @@ const Header = () => {
   }, [router]);
 
   return (
-    <div className="mx-auto flex flex-wrap p-5 md:flex-col text-[17px] text-white flex-row items-center justify-between w-full font-space-grotesk">
+    <div className="flex flex-wap p-5 md:flex-col md:justify-center md:items-center sm:flex-col sm:justify-center sm:items-center text-[17px] text-white flex-row items-center justify-between  font-space-grotesk">
       <img
-        className="min-w-[100px] w-[200px] cursor-pointer"
+        className="min-w-[100px] w-auto cursor-pointer"
         alt=""
         src="../hacknitp57.svg"
         onClick={onLogoClick}
       />
 
-      <nav className="flex flex-wrap justify-center items-center md:mt-4 text-base">
-        <b className="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={onAboutUsClick}>About Us</b>
-        <b className="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={onTimelineTextClick}>Timeline</b>
-        <b className="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={onRulesTextClick}>Rules</b>
-        <b className="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={onSponsorsTextClick}>Sponsors</b>
-        <b className="mr-5 cursor-pointer md:mt-1 sm:mt-1" onClick={onContactUsTextClick}>Contact Us</b>
+      <nav className="flex mt-1 flex-wrap justify-center items-center text-xs gap-x-5 gap-y-1 font-semibold md:text-[16px] sm:text-[16px]">
+        <b className="cursor-pointer" onClick={onAboutUsClick}>About Us</b>
+        <b className="cursor-pointer" onClick={onTimelineTextClick}>Timeline</b>
+        <b className="cursor-pointer" onClick={onRulesTextClick}>Rules</b>
+        <b className="cursor-pointer" onClick={onSponsorsTextClick}>Sponsors</b>
+        <b className="cursor-pointer" onClick={onContactUsTextClick}>Contact Us</b>
         <a href="https://bit.ly/hacknitp-4-discord" target={"_blank"} rel="noreferrer noopener">
-          <button className="border-0 font-bold py-2 px-5 text-xs cursor-pointer focus:outline-none rounded-xl  md:mt-1 sm:mt-1 text-black bg-[#2BFF08]">
+          <button className="border-0 font-semibold py-2 px-5 text-xs cursor-pointer md:text-[16px] sm:text-[16px] focus:outline-none rounded-xl  md:mt-1 sm:mt-1 text-black bg-[#2BFF08]">
             Join Discord
           </button>
         </a>
