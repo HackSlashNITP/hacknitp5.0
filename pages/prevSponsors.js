6539a22bb1aa40396994453709ef9e98025b670c
RitwikSingh28 <itsritwik26@gmail.com>
2022-12-27 11:19:55 +0530

    link prevSponsors to website

---
+++ pages/prevSponsors.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/header';
+import Footer from '../components/footer';
+
+const PrevSponsors = () => {
+  return (
+    <div className='bg-black w-full h-screen'>
+        <Header />
+
+        <Footer />
+    </div>
+  )
+}
+
+export default PrevSponsors;
