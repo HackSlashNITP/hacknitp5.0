3cf324bbc36bcbdb62f4b87f6d5a6be115b09e18
RitwikSingh28 <itsritwik26@gmail.com>
2022-12-28 17:53:52 +0530

    optional layout added for organizer display

--- components/organizers/organizerCard.js
+++ components/organizers/organizerCard.js
@@ -4,20 +4,55 @@ import style from "../../styles/organisers.module.css";
 import SocialMediaIcon from "../social_media_icon";
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
-const OrganizerCard = ({imgUrl, name, role, linkedin, github}) => {
+const OrganizerCard = ({ imgUrl, name, role, linkedin, github }) => {
   return (
-    <div className="m-4 ">
+    // Responsive, adapts to different name sizes, to be used when all social media handles are available
+    // <div className="m-4 h-[285px] flex flex-col justify-between">
+    //   <div className={style.container}>
+    //     <Image
+    //       src={imgUrl}
+    //       width={125}
+    //       height={125}
+    //       objectFit="cover"
+    //       className="rounded-full sm:w-[100px] sm:h-[100px] [border:2px_solid_transparent] hover:[border:2px_solid_#2BFF08]"
+    //       id="pic"
+    //     />
+    //     <figcaption
+    //       className="text-xs text-yellow-900"
+    //       style={{ fontSize: "1.2rem" }}
+    //     >
+    //       {name}
+    //     </figcaption>
+    //   </div>
+
+    //   <div className="details flex flex-col gap-y-0">
+    //     <figcaption
+    //       className="p-0 m-0 text-[#4ade80]"
+    //       style={{ fontSize: "0.8rem" }}
+    //     >
+    //       {role}
+    //     </figcaption>
+
+    //     <div className="social-media-handles flex flex-row gap-x-4 justify-center items-center">
+    //       {linkedin && (
+    //         <SocialMediaIcon link={linkedin} icon={<AiFillLinkedin />} />
+    //       )}
+    //       {github && <SocialMediaIcon link={github} icon={<AiFillGithub />} />}
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="m-4">
       <div className={style.container}>
         <Image
           src={imgUrl}
           width={125}
           height={125}
-          objectFit='cover'
-          className="border-2 rounded-full sm:w-[100px] sm:h-[100px]"
+          objectFit="cover"
+          className="rounded-full sm:w-[100px] sm:h-[100px] [border:2px_solid_transparent] hover:[border:2px_solid_#2BFF08]"
           id="pic"
         />
       </div>
-
       <figcaption
         className="text-xs text-yellow-900"
         style={{ fontSize: "1.2rem" }}
@@ -32,9 +67,11 @@ const OrganizerCard = ({imgUrl, name, role, linkedin, github}) => {
         {role}
       </figcaption>
 
-      <div className="social-media-handles flex flex-row gap-x-3 justify-center items-center">
-        {linkedin && <SocialMediaIcon link={linkedin} icon={<AiFillLinkedin />}/>}
-        {github && <SocialMediaIcon link={github} icon={<AiFillGithub />}/>}
+      <div className="social-media-handles flex flex-row gap-x-4 justify-center items-center">
+        {linkedin && (
+          <SocialMediaIcon link={linkedin} icon={<AiFillLinkedin />} />
+        )}
+        {github && <SocialMediaIcon link={github} icon={<AiFillGithub />} />}
       </div>
     </div>
   );
