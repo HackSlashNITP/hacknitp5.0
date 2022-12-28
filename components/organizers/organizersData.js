0e0e18b9370b1b3bd6c9405362bb743dbd2acb3f
RitwikSingh28 <itsritwik26@gmail.com>
2022-12-28 12:56:47 +0530

    modify organizer data to contain social media handle

--- components/organizers/organizersData.js
+++ components/organizers/organizersData.js
@@ -1,271 +1,422 @@
-import Shivam from '../../public/organizers/Shivam Jha.jpg';
-import Saizal from '../../public/organizers/Saizal Sinha.jpg';
-import Tejas from '../../public/organizers/Tejas Manhas.jpg';
-import Pulkit from '../../public/organizers/Pulkit Agarwal.jpeg';
+import Shivam from "../../public/organizers/Shivam Jha.jpg";
+import Saizal from "../../public/organizers/Saizal Sinha.jpg";
+import Tejas from "../../public/organizers/Tejas Manhas.jpg";
+import Pulkit from "../../public/organizers/Pulkit Agarwal.jpeg";
 // import PragatiV from "../../public/organizers/Pragati Verma.jpg"
 // import Tanvi from '../../public/organizers/Tanvi Tanya.heic';
-import Srijan from '../../public/organizers/Srijan Shovit.png';
-import Paras from '../../public/organizers/Paras punjabi.jpg';
-import Harsh from '../../public/organizers/Harsh Anand.JPG';
-import Priyanshu from '../../public/organizers/Priyanshu Shekhar.jpg';
-import Rishav from '../../public/organizers/RISHAV JAIN.jpeg';
-import PragatiP from '../../public/organizers/Pragati Priya.jpg';
-import Mohit from '../../public/organizers/Mohit Kumar.jpg';
-import Jatin from '../../public/organizers/Jatin Chauhan.png';
-import Kalpesh from '../../public/organizers/Kalpesh_Meena.jpg';
-import Ayush from '../../public/organizers/Ayush Gautam.jpg';
-import Shashank from '../../public/organizers/Shashank Yadav.jpg';
-import Shrey from '../../public/organizers/Shrey Kashyap.jpg';
-import Devatraj from '../../public/organizers/Devatraj Bhattacharya.jpeg';
-import Sakshi from '../../public/organizers/Sakshi Singh.jpg';
-import Ashutosh from '../../public/organizers/Ashutosh Bind.png';
-import Manvendra from '../../public/organizers/Manvendra_Pratap_Singh.jpeg';
-import Rani from '../../public/organizers/Rani Kumari.jpg';
-import Susajjit from '../../public/organizers/Susajjit Singh.png';
-import Ritwik from '../../public/organizers/Ritwik Singh.jpg';
-import Chetan from '../../public/organizers/CHETAN SRIVASTAVA.JPG';
-import Amisha from '../../public/organizers/Amisha Bharti.jpg';
-import Vishal from '../../public/organizers/vishal_gaurav.jpg';
-import Deepak from '../../public/organizers/Deepak Kumar.jpeg';
-import Sameer from '../../public/organizers/Sameer Choudhary.jpg';
-import Rohan from '../../public/organizers/Rohan Shetty.png';
-import Nisarg from '../../public/organizers/Nisarg Patel.jpg';
-import Mandeep from '../../public/organizers/Mandeep_Singh_Saluja.png';
-import RohanS from '../../public/organizers/Rohan Shetty.png';
-import Ifra from '../../public/organizers/Ifra_Ali.png';
-import Sachin from '../../public/organizers/Sachin_Kumar.png';
-import Sanya from '../../public/organizers/Sanya Kumari.jpeg';
-import Simranpreet from '../../public/organizers/Simranpreet Kaur.jpg';
+import Srijan from "../../public/organizers/Srijan Shovit.png";
+import Paras from "../../public/organizers/Paras punjabi.jpg";
+import Harsh from "../../public/organizers/Harsh Anand.JPG";
+import Priyanshu from "../../public/organizers/Priyanshu Shekhar.jpg";
+import Rishav from "../../public/organizers/RISHAV JAIN.jpeg";
+import PragatiP from "../../public/organizers/Pragati Priya.jpg";
+import Mohit from "../../public/organizers/Mohit Kumar.jpg";
+import Jatin from "../../public/organizers/Jatin Chauhan.png";
+import Kalpesh from "../../public/organizers/Kalpesh_Meena.jpg";
+import Ayush from "../../public/organizers/Ayush Gautam.jpg";
+import Shashank from "../../public/organizers/Shashank Yadav.jpg";
+import Shrey from "../../public/organizers/Shrey Kashyap.jpg";
+import Devatraj from "../../public/organizers/Devatraj Bhattacharya.jpeg";
+import Sakshi from "../../public/organizers/Sakshi Singh.jpg";
+import Ashutosh from "../../public/organizers/Ashutosh Bind.png";
+import Manvendra from "../../public/organizers/Manvendra_Pratap_Singh.jpeg";
+import Rani from "../../public/organizers/Rani Kumari.jpg";
+import Susajjit from "../../public/organizers/Susajjit Singh.png";
+import Ritwik from "../../public/organizers/Ritwik Singh.jpg";
+import Chetan from "../../public/organizers/CHETAN SRIVASTAVA.JPG";
+import Amisha from "../../public/organizers/Amisha Bharti.jpg";
+import Vishal from "../../public/organizers/vishal_gaurav.jpg";
+import Deepak from "../../public/organizers/Deepak Kumar.jpeg";
+import Sameer from "../../public/organizers/Sameer Choudhary.jpg";
+import Rohan from "../../public/organizers/Rohan Shetty.png";
+import Nisarg from "../../public/organizers/Nisarg Patel.jpg";
+import Mandeep from "../../public/organizers/Mandeep_Singh_Saluja.png";
+import RohanS from "../../public/organizers/Rohan Shetty.png";
+import Ifra from "../../public/organizers/Ifra_Ali.png";
+import Sachin from "../../public/organizers/Sachin_Kumar.png";
+import Sanya from "../../public/organizers/Sanya Kumari.jpeg";
+import Simranpreet from "../../public/organizers/Simranpreet Kaur.jpg";
+import Anushka from "../../public/organizers/anushka dubey.jpg";
+import Pawni from "../../public/organizers/pawni chauhan.jpg";
+import AnshuG from "../../public/organizers/anshu gupta.jpg";
+import Avinash from "../../public/organizers/Avinash Kumar.jpg";
 
 const organizersList = [
-    {
-        'id': 0,
-        'imgUrl': Shivam.src,
-        'name': 'Shivam Jha',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 1,
-        'imgUrl': Saizal.src,
-        'name': 'Saizal Sinha',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 2,
-        'imgUrl': Tejas.src,
-        'name': 'Tejas Manhas',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 3,
-        'imgUrl': Pulkit.src,
-        'name': 'Pulkit Agarwal',
-        'role': 'Event Management Lead'
-    },
-    // {
-    //     'id': 4,
-    //     'imgUrl': Shivam.src,
-    //     'name': 'Rahul',
-    //     'role': 'Event Management Lead'
-    // },
-    // {
-    //     'id': 5,
-    //     'imgUrl': Tanvi.src,
-    //     'name': 'Tanvi Tanya',
-    //     'role': 'Event Management Lead'
-    // },
-    {
-        'id': 6,
-        'imgUrl': Srijan.src,
-        'name': 'Srijan Shovit',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 7,
-        'imgUrl': Paras.src,
-        'name': 'Paras Punjabi',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 8,
-        'imgUrl': Harsh.src,
-        'name': 'Harsh Anand',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 9,
-        'imgUrl': Priyanshu.src,
-        'name': 'Priyanshu Shekhar',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 10,
-        'imgUrl': Rishav.src,
-        'name': 'Rishav Jain',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 11,
-        'imgUrl': PragatiP.src,
-        'name': 'Pragati Priya',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 12,
-        'imgUrl': Mohit.src,
-        'name': 'Mohit Kumar',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 13,
-        'imgUrl': Jatin.src,
-        'name': 'Jatin Chauhan',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 14,
-        'imgUrl': Kalpesh.src,
-        'name': 'Kalpesh Meena',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 15,
-        'imgUrl': Ayush.src,
-        'name': 'Ayush Gautam',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 16,
-        'imgUrl': Shashank.src,
-        'name': 'Shashank Yadav',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 17,
-        'imgUrl': Shrey.src,
-        'name': 'Shrey Kashyap',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 18,
-        'imgUrl': Devatraj.src,
-        'name': 'Devatraj Bhattarcharya',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 19,
-        'imgUrl': Sakshi.src,
-        'name': 'Sakshi Singh',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 20,
-        'imgUrl': Ashutosh.src,
-        'name': 'Ashutosh Bind',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 21,
-        'imgUrl': Manvendra.src,
-        'name': 'Manvendra Pratap Singh',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 22,
-        'imgUrl': Rani.src,
-        'name': 'Rani Kumari',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 23,
-        'imgUrl': Susajjit.src,
-        'name': 'Susajjit Kumar Singh',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 24,
-        'imgUrl': Ritwik.src,
-        'name': 'Ritwik Singh',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 25,
-        'imgUrl': Chetan.src,
-        'name': 'Chetan Srivastava',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 26,
-        'imgUrl': Amisha.src,
-        'name': 'Amisha Bharti',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 27,
-        'imgUrl': Vishal.src,
-        'name': 'Vishal Gaurav',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 28,
-        'imgUrl': Deepak.src,
-        'name': 'Deepak Kumar',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 29,
-        'imgUrl': Sameer.src,
-        'name': 'Sameer Choudhary',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 30,
-        'imgUrl': Rohan.src,
-        'name': 'Rohan Choudhary',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 31,
-        'imgUrl': Nisarg.src,
-        'name': 'Nisarg Patel',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 32,
-        'imgUrl': Mandeep.src,
-        'name': 'Mandeep Singh Saluja',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 33,
-        'imgUrl': RohanS.src,
-        'name': 'Rohan Shetty',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 34,
-        'imgUrl': Ifra.src,
-        'name': 'Ifra Ali',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 35,
-        'imgUrl': Sachin.src,
-        'name': 'Sachin Kumar',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 36,
-        'imgUrl': Sanya.src,
-        'name': 'Sanya Kumari',
-        'role': 'Event Management Lead'
-    },
-    {
-        'id': 37,
-        'imgUrl': Simranpreet.src,
-        'name': 'Simranpreet Kaur',
-        'role': 'Event Management Lead'
-    }
+  {
+    id: 0,
+    imgUrl: Shivam.src,
+    name: "Shivam Jha",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 1,
+    imgUrl: Saizal.src,
+    name: "Saizal Sinha",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/saizal",
+    github: "https://github.com/Saizalsinha",
+    twitter: "",
+  },
+  {
+    id: 2,
+    imgUrl: Tejas.src,
+    name: "Tejas Manhas",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 3,
+    imgUrl: Pulkit.src,
+    name: "Pulkit Agarwal",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  // {
+  //     'id': 4,
+  //     'imgUrl': PragatiV.src,
+  //     'name': 'Pragati Verma',
+  //     'role': 'Event Management Lead',
+  //     'linkedin': 'https://www.linkedin.com/in/pragati-verma-654639212',
+  //     'github': 'https://github.com/Pragati1122',
+  //     'twitter': ''
+  // },
+  // {
+  //     'id': 5,
+  //     'imgUrl': Tanvi.src,
+  //     'name': 'Tanvi Tanya',
+  //     'role': 'Event Management Lead'
+  // },
+  {
+    id: 6,
+    imgUrl: Srijan.src,
+    name: "Srijan Shovit",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/srijan-shovit-6b3b131ba",
+    github: "https://github.com/SrijanShovit",
+    twitter: "https://twitter.com/shovitsrijan",
+  },
+  {
+    id: 7,
+    imgUrl: Paras.src,
+    name: "Paras Punjabi",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/paras-punjabi-0b7669220/",
+    github: "https://github.com/Paras-Punjabi",
+    twitter: "",
+  },
+  {
+    id: 8,
+    imgUrl: Harsh.src,
+    name: "Harsh Anand",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/devharsh01",
+    github: "https://github.com/its-me-Harsh-Anand",
+    twitter: "",
+  },
+  {
+    id: 9,
+    imgUrl: Priyanshu.src,
+    name: "Priyanshu Shekhar",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 10,
+    imgUrl: Rishav.src,
+    name: "Rishav Jain",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 11,
+    imgUrl: PragatiP.src,
+    name: "Pragati Priya",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 12,
+    imgUrl: Mohit.src,
+    name: "Mohit Kumar",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/mohit-kumar-02b07667",
+    github: "https://github.com/mohit-kumar-b2",
+    twitter: "",
+  },
+  {
+    id: 13,
+    imgUrl: Jatin.src,
+    name: "Jatin Chauhan",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 14,
+    imgUrl: Kalpesh.src,
+    name: "Kalpesh Meena",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 15,
+    imgUrl: Ayush.src,
+    name: "Ayush Gautam",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/ayushgautam07",
+    github: "https://github.com/ayushgautam7",
+    twitter: "",
+  },
+  {
+    id: 16,
+    imgUrl: Shashank.src,
+    name: "Shashank Yadav",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/shashank-yadav-294092230",
+    github: "https://github.com/Shashank2104125",
+    twitter: "https://twitter.com/Shashanky01",
+  },
+  {
+    id: 17,
+    imgUrl: Shrey.src,
+    name: "Shrey Kashyap",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/shrey-kashyap-a47b67240",
+    github: "https://github.com/shreyneol",
+    twitter: "",
+  },
+  {
+    id: 18,
+    imgUrl: Devatraj.src,
+    name: "Devatraj Bhattarcharya",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/devatraj-bhattacharya-6a22611b4",
+    github: "https://www.github.com/devatraj",
+    twitter: "",
+  },
+  {
+    id: 19,
+    imgUrl: Sakshi.src,
+    name: "Sakshi Singh",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/sakshi-singh-02658a22b",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 20,
+    imgUrl: Ashutosh.src,
+    name: "Ashutosh Bind",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 21,
+    imgUrl: Manvendra.src,
+    name: "Manvendra Pratap Singh",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 22,
+    imgUrl: Rani.src,
+    name: "Rani Kumari",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/rani-kumari-a74915229/",
+    github: "https://github.com/rani620",
+    twitter: "https://twitter.com/rani6201",
+  },
+  {
+    id: 23,
+    imgUrl: Susajjit.src,
+    name: "Susajjit Kumar Singh",
+    role: "Event Management Lead",
+    linkedin: "http://www.linkedin.com/in/susajjit",
+    github: "https://github.com/SUSAJJIT",
+    twitter: "",
+  },
+  {
+    id: 24,
+    imgUrl: Ritwik.src,
+    name: "Ritwik Singh",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 25,
+    imgUrl: Chetan.src,
+    name: "Chetan Srivastava",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 26,
+    imgUrl: Amisha.src,
+    name: "Amisha Bharti",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 27,
+    imgUrl: Vishal.src,
+    name: "Vishal Gaurav",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 28,
+    imgUrl: Deepak.src,
+    name: "Deepak Kumar",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 29,
+    imgUrl: Sameer.src,
+    name: "Sameer Choudhary",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/sameer-choudhary-2255ab222",
+    github: "https://github.com/samzorroak",
+    twitter: "https://twitter.com/sam_zorroak?t=0N_6eYGYjk8GOXMZN0Hfyg&s=09",
+  },
+  {
+    id: 30,
+    imgUrl: Rohan.src,
+    name: "Rohan Choudhary",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/rohan-kumar-choudhary-2ab819226",
+    github: "https://github.com/RohanKumarChoudhary",
+    twitter: "",
+  },
+  {
+    id: 31,
+    imgUrl: Nisarg.src,
+    name: "Nisarg Patel",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/nisarg-patel-293454229/",
+    github: "https://www.linkedin.com/in/nisarg-patel-293454229/",
+    twitter: "https://twitter.com/Nisarg2003",
+  },
+  {
+    id: 32,
+    imgUrl: Mandeep.src,
+    name: "Mandeep Singh Saluja",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/mandeep-singh-saluja-70a695231",
+    github: "https://github.com/MandeepSaluja",
+    twitter: "https://mobile.twitter.com/mindtheman",
+  },
+  {
+    id: 33,
+    imgUrl: RohanS.src,
+    name: "Rohan Shetty",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 34,
+    imgUrl: Ifra.src,
+    name: "Ifra Ali",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 35,
+    imgUrl: Sachin.src,
+    name: "Sachin Kumar",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/public-profile/settings",
+    github: "https://github.com/sAchin12320-web",
+    twitter: "",
+  },
+  {
+    id: 36,
+    imgUrl: Sanya.src,
+    name: "Sanya Kumari",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/sanya-kumari-8ba878228",
+    github: "https://www.github.com/Sanaya56",
+    twitter: "",
+  },
+  {
+    id: 37,
+    imgUrl: Simranpreet.src,
+    name: "Simranpreet Kaur",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/simranpreet-kaur-8b669b238",
+    github: "https://github.com/03Simran",
+    twitter: "",
+  },
+  {
+    id: 38,
+    imgUrl: AnshuG.src,
+    name: "Anshu Gupta",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/anshu-gupta-66519122a/",
+    github: "https://github.com/Anshu1Gupta",
+    twitter: "https://twitter.com/rahul10096218",
+  },
+  {
+    id: 39,
+    imgUrl: Anushka.src,
+    name: "Anushka Dubey",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/anushka-d-751b20229",
+    github: "https://github.com/anush07k",
+    twitter: "https://twitter.com/nushk0807",
+  },
+  {
+    id: 40,
+    imgUrl: Pawni.src,
+    name: "Pawni Chauhan",
+    role: "Event Management Lead",
+    linkedin: "",
+    github: "",
+    twitter: "",
+  },
+  {
+    id: 41,
+    imgUrl: Avinash.src,
+    name: "Avinash Kumar",
+    role: "Event Management Lead",
+    linkedin: "https://www.linkedin.com/in/avinash-kumar-0a66a782/",
+    github: "https://github.com/Avik17",
+    twitter: "https://twitter.com/avik_1706",
+  },
 ];
 
-export default organizersList; 
+export default organizersList;
