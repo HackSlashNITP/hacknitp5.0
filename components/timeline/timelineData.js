2fd613a2dd1f3c1995405c9d8d8eb90e62ad7efc
Shashank Yadav <97789487+Shashank2104125@users.noreply.github.com>
2023-03-10 23:31:16 +0530

    updated timeline

--- components/timeline/timelineData.js
+++ components/timeline/timelineData.js
@@ -1,58 +1,58 @@
 const data = [
     {
         id: 0,
-        title: 'Registration Starts',
-        schedule: '10 January, 18:00 IST',
+        title: 'Hackathon Registration Starts',
+        schedule: '15 March 2023, 18:00 IST',
     },
     {
         id: 1,
         title: 'Info Session',
-        schedule: '21 January, 16:00 IST',
+        schedule: '01 April 2023, To be decided',
     },
     {
         id: 2,
         title: 'Workshop Series Starts',
-        schedule: '4 February',
+        schedule: '07 April 2023',
     },
     {
         id: 3,
         title: 'Workshop Series Ends',
-        schedule: '10 February',
+        schedule: '13 April 2023',
     },
     {
         id: 4,
         title: 'Hackathon Registration Ends',
-        schedule: '10 February, 21:00 IST',
+        schedule: '13 April 2023, 21:00 IST',
     },
     {
         id: 5,
         title: 'Opening Ceremony',
-        schedule: '11 February, 10:00 IST',
+        schedule: '14 April 2023, 10:00 IST',
     },
     {
         id: 6,
         title: 'Hackathon Starts',
-        schedule: '11 February, 12:00 IST',
+        schedule: '14 April 2023, 12:00 IST',
     },
     {
         id: 7,
         title: 'Hackathon Ends',
-        schedule: '12 February, 20:00 IST',
+        schedule: '15 April 2023, 20:00 IST',
     },
     {
         id: 8,
         title: 'Community Review Window Starts',
-        schedule: '12 February, 21:00 IST',
+        schedule: '15 April 2023, 21:00 IST',
     },
     {
         id: 9,
         title: 'Community Review Window Ends',
-        schedule: '13 February, 21:00 IST',
+        schedule: '16 April 2023, 21:00 IST',
     },
     {
         id: 10,
         title: 'Closing Ceremony',
-        schedule: '19 February, 18:00 IST',
+        schedule: '22 April 2023, 18:00 IST',
     },
 ];
 
