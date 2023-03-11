760586dc2758751e84186074a9a2c990096b46b2
Shashank Yadav <97789487+Shashank2104125@users.noreply.github.com>
2023-03-12 01:35:18 +0530

    updating api session section

--- components/apidays.js
+++ components/apidays.js
@@ -10,9 +10,30 @@ const Apidays = () => {
       <div className={styles.timeline}>
         <div className={styles.heading}> API-Days Patna Schedule</div>
         <div className={styles.outer}>
-          {apiData.map((datum) => {
-            return <SchedulingCard key={datum.id} title={datum.title} schedule={datum.schedule}/>
-          })}
+         
+        <div className={styles.card}>
+       <div className={styles.info}>
+        <h3 className={styles.title}>Day 0</h3>
+        <p>Online Student Expert Training Session</p>
+      </div>
+    </div>
+
+    <div className={styles.card}>
+       <div className={styles.info}>
+        <h3 className={styles.title}>Day 1</h3>
+        <p>- APIs 101</p>
+        <p>- APIs 102</p>
+      </div>
+    </div>
+
+    <div className={styles.card}>
+       <div className={styles.info}>
+        <h3 className={styles.title}>Day 2</h3>
+        <p>- APIs 103(Integrated MongoDB Data API and automated testing).</p>
+        <p>- Securely Package and Deploy your APIs with Docker and Docker Compose.</p>
+      </div>
+    </div>
+
         </div>
       </div>
     </section>
