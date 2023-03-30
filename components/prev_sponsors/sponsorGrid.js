1bf6bd1a1652a5bd1dd0904298176a07dec991bc
Paras Punjabi <paraspunjabi2002@gmail.com>
2023-03-30 14:38:35 +0530

    updated styling of sponsor box

--- components/prev_sponsors/sponsorGrid.js
+++ components/prev_sponsors/sponsorGrid.js
@@ -5,7 +5,7 @@ const SponsorGrid = ({ sponsors, title }) => {
   return (
     <section className="title-sponsor w-full flex flex-col items-center">
       <h4 className="text-2xl font-normal my-5 md:text-lg md:my-3">{title}</h4>
-      <div className="w-[75%] md:w-[100%] grid grid-cols-3 gap-4 md:grid-cols-1 md:gap-2 place-items-center">
+      <div className="w-[75%] md:w-[100%] flex flex-row justify-center items-center gap-16 flex-wrap">
         {sponsors.map((sponsor) => {
           return (
             <SponsorBox
